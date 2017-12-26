70810f9fbba7c1ccf8598ae03ca54b41f2b35dd9
rajib.bhowmik <rajib.bhowmik@gmail.com>
2017-12-26 19:18:18 +0100

    -Validation added and tested

--- AddressBook/Models/Address.cs
+++ AddressBook/Models/Address.cs
@@ -9,19 +9,28 @@ namespace AddressBook.Models
     public class Address
     {
         public int AddressID { get; set; }
+
+        [RegularExpression(@"\+?(?:0{0,2}[46]*){1}7{1}[0-9]{8}", ErrorMessage = "Typing format: +46xxxxxxxxx")]
         [Required]
+        [Display(Name = "Phone Number")]
         public string PhoneNumber { get; set; }
        
+        [RegularExpression(@"\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*", ErrorMessage = "Invalid Email Address")]
+        [Display(Name = "Email Address")]
         public string Email { get; set; }
         [Required]
+        [Display(Name = "Street")]
         public string StreetName { get; set; }
         [Required]
+        [Display(Name = "House No.")]
         public string HomeNumber { get; set; }
         [Required]
+        [Display(Name = "Post Code")]
         public string PostalCode { get; set; }
         [Required]
         public string City { get; set; }
 
+        
         public int PersonID { get; set; }
         public Person Person { get; set; }
     }
--- AddressBook/Models/Person.cs
+++ AddressBook/Models/Person.cs
@@ -8,11 +8,16 @@ namespace AddressBook.Models
 {
     public class Person
     {
-        public int PersonID { get; set; }    
-        
+        public int PersonID { get; set; }
+
+        [StringLength(60, MinimumLength = 2)]
         [Required(ErrorMessage = "Please write your first name")]
+        [Display(Name = "First Name")]
         public string FirstName { get; set; }
+
+        [StringLength(60, MinimumLength = 2)]
         [Required(ErrorMessage = "Please write your last name")]
+        [Display(Name = "Last Name")]
         public string LastName { get; set; }
 
         public ICollection<Address> Addresses { get; set; }
