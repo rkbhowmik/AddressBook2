599d16a6e94448e99a408a1f9d82c2bef02b6441
rajib.bhowmik <rajib.bhowmik@gmail.com>
2017-12-26 01:16:37 +0100

    -People Controller added

--- AddressBook/Data/AddressBookContext.cs
+++ AddressBook/Data/AddressBookContext.cs
@@ -3,16 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
-using AddressBook.Models;
 
 namespace AddressBook.Models
 {
-    public class AddressBookContext: DbContext
+    public class AddressBookContext : DbContext
     {
-        public AddressBookContext(DbContextOptions<AddressBookContext> options) : base(options)
+        public AddressBookContext (DbContextOptions<AddressBookContext> options)
+            : base(options)
         {
-            
         }
-        public DbSet<Person> Persons { get; set; }
+
+        public DbSet<AddressBook.Models.Person> Person { get; set; }
     }
 }
--- AddressBook/Startup.cs
+++ AddressBook/Startup.cs
@@ -6,6 +6,8 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using AddressBook.Models;
 
 namespace AddressBook
 {
@@ -22,6 +24,9 @@ namespace AddressBook
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
+
+            services.AddDbContext<AddressBookContext>(options =>
+                    options.UseSqlServer(Configuration.GetConnectionString("AddressBookContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
