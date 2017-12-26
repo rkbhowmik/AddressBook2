10c0a07e412a8604605db82f1c7b88bddf8fc7b4
rajib.bhowmik <rajib.bhowmik@gmail.com>
2017-12-26 01:21:20 +0100

    -Address Model Added

---
+++ AddressBook/Models/Address.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AddressBook.Models
+{
+    public class Address
+    {
+        public int AddressID { get; set; }
+
+        public string PhoneNumber { get; set; }
+        public string Email { get; set; }
+        public string StreetName { get; set; }
+        public string HomeNumber { get; set; }
+
+        public int PostelCode { get; set; }
+        public string City { get; set; }
+    }
+}
