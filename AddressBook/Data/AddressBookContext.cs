32203ac00b542f0e49f9d06ee7ca545c834cafc7
rajib.bhowmik <rajib.bhowmik@gmail.com>
2017-12-26 01:07:00 +0100

    -Person Model added

---
+++ AddressBook/Data/AddressBookContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using AddressBook.Models;
+
+namespace AddressBook.Models
+{
+    public class AddressBookContext: DbContext
+    {
+        public AddressBookContext(DbContextOptions<AddressBookContext> options) : base(options)
+        {
+            
+        }
+        public DbSet<Person> Persons { get; set; }
+    }
+}
