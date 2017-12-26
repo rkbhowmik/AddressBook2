434d3dd93f242aac050e3f604661a82f053fc89b
rajib.bhowmik <rajib.bhowmik@gmail.com>
2017-12-26 02:28:59 +0100

    -Added Address migration -layout changed - Person & Address tab added

--- AddressBook/Migrations/20171226012235_AddedAddress.Designer.cs
+++ AddressBook/Migrations/20171226012235_AddedAddress.Designer.cs
@@ -11,9 +11,10 @@ using System;
 namespace AddressBook.Migrations
 {
     [DbContext(typeof(AddressBookContext))]
-    partial class AddressBookContextModelSnapshot : ModelSnapshot
+    [Migration("20171226012235_AddedAddress")]
+    partial class AddedAddress
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ AddressBook/Migrations/20171226012235_AddedAddress.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace AddressBook.Migrations
+{
+    public partial class AddedAddress : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "ID",
+                table: "Person",
+                newName: "PersonID");
+
+            migrationBuilder.CreateTable(
+                name: "Address",
+                columns: table => new
+                {
+                    AddressID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    City = table.Column<string>(nullable: true),
+                    Email = table.Column<string>(nullable: true),
+                    HomeNumber = table.Column<string>(nullable: true),
+                    PersonID = table.Column<int>(nullable: false),
+                    PhoneNumber = table.Column<string>(nullable: true),
+                    PostalCode = table.Column<int>(nullable: false),
+                    StreetName = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Address", x => x.AddressID);
+                    table.ForeignKey(
+                        name: "FK_Address_Person_PersonID",
+                        column: x => x.PersonID,
+                        principalTable: "Person",
+                        principalColumn: "PersonID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Address_PersonID",
+                table: "Address",
+                column: "PersonID");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Address");
+
+            migrationBuilder.RenameColumn(
+                name: "PersonID",
+                table: "Person",
+                newName: "ID");
+        }
+    }
+}
